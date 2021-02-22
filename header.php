74a0295e033833497d3fb5e398f8643201add9c7
jhon724 <78187800+jhon724@users.noreply.github.com>
2021-02-22 21:30:53 +0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="top-navbar">
+	<center>
+		
+	    <img src="logo.jpg" height="50px" width="90px;">
+	</center>
+	<div class="search-bar">	
+		<input type="text" name="" placeholder="Search">
+		<button>Ok</button>
+	</div>
+</div>
+<div class="side-bar">
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Trending</a></li>
+			<li><a href="">Subscription</a></li>
+			<hr>
+			<li><a href="">Library</a></li>
+			<li><a href="">History</a></li>
+			<li><a href="">Your videos</a></li>
+			<li><a href="">Watch later</a></li>
+			<li><a href="">Adobe Premiere Pro...</a></li>
+			<hr>
+			<li><a href="">Gaming</a></li>
+			<li><a href="">Live</a></li>
+			<li><a href="">Support</a></li>
+			<hr>
+			<li><a href="">Settings</a></li>
+			<li><a href="">Report history</a></li>
+			<li><a href="">Help</a></li>
+			<li><a href="">Send feedback</a></li>
+			<hr>
+		</ul>	
+		<a href="">About Press</a>
+		<a href="">Copy Right</a>
+		<a href="">Contact Us</a>
+		<a href="">Creators</a>
+</div>
---
+++ work/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Geniune tech</title>
+</head>
+<body>
+	<?php include 'header.php'; ?>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<div class="mydiv">
+		
+	</div>
+	<?php include 'footer.php'; ?>
+</body>
+</html>
